f62031b8e16e9be429c7c4294be7b9e324e83993
Dooshort <fafalnq@gmail.com>
2022-04-22 00:14:19 -0400

    Chair Sitting Interaction "Code Smells" fix

--- Assets/Scripts/SitInteraction.cs
+++ Assets/Scripts/SitInteraction.cs
@@ -24,10 +24,26 @@ public class SitInteraction : InteractableObject
     }
 
     public override void HandleInteraction() {
-        player.isSitting = true;
-        this.GetComponent<Collider>().enabled = false;
-        player.transform.eulerAngles = Vector3.zero;
-        player.transform.position = new Vector3(sitPivot.position.x, player.transform.position.y, sitPivot.position.z);
+        if(player.isSitting) {
+            Debug.Log("Standing up");
+            // TODO: Set player move condition to True
+            // player.SetCanMove(true);
+            player.isSitting = false;
+            player.destination = player.transform.position+new Vector3(0, 0, 1f);
+            SelectionManager selectionManager = player.GetComponentInChildren<SelectionManager>();
+            selectionManager.ResetSelection();
+        }
+        else {
+            Debug.Log("Sitting down");
+            // TODO: Set player move condition to False
+            // TODO: Change player direction when sitting down (and standing up if needed)
+            // player.SetCanMove(false);
+            player.isSitting = true;
+            player.direction = new Vector3(0, 0, 1);
+            this.GetComponent<Collider>().enabled = false;
+            player.transform.eulerAngles = Vector3.zero;
+            player.transform.position = new Vector3(sitPivot.position.x, player.transform.position.y, sitPivot.position.z);
+        }
     }
     
 }
